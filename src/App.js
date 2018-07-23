7fe07081b76cfd492e046b4d0a0041a719cd8ced
Rimgaile Sinkeviciene <rimgaile.sinkeviciene@gmail.com>
2018-07-23 23:29:34 +0300

    Add new components for search page and book

--- src/App.js
+++ src/App.js
@@ -1,56 +1,31 @@
-import React from 'react'
-// import * as BooksAPI from './BooksAPI'
-import { Link } from 'react-router-dom'
-import { Route } from 'react-router-dom'
-import './App.css'
-import BooksList from './BooksList'
+import React from 'react';
+import * as BooksAPI from './BooksAPI';
+import { Route } from 'react-router-dom';
+import { PropTypes } from 'prop-types';
+import './App.css';
+import BooksList from './BooksList';
+import BooksSearch from './BooksSearch'
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
 
+		
   render() {
     return (
       <div className="app">
 		<Route exact path='/search' render={() => (
-	       <div className="search-books">
-            <div className="search-books-bar">
-              <Link to='/' className="close-search">Close</Link>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-       		</div>
-	
-	)}/>
-   	<Route exact path='/' render={() => (
-			<BooksList />		  
-								  
-	)}/>
-			
-        
+			<BooksSearch />
+		)}/>
+   		<Route exact path='/' render={() => (
+			<BooksList />		  						  
+		)}/>	  
       </div>
     )
   }
 }
+
+/*BooksApp.propTypes = {
+	//add proptypes
+	updateQuery: PropTypes.func.isRequired
+}*/
 
 export default BooksApp
